3e481d78d16b9bb92d58770317ea951cf6155b8a
sukan <sukan.stbsr@gmail.com>
2019-07-10 21:23:20 +0700

    patternGenerateGetAndSet

--- src/main/java/com/captcha/CAPTCHA/Captcha.java
+++ src/main/java/com/captcha/CAPTCHA/Captcha.java
@@ -1,9 +1,80 @@
 package com.captcha.CAPTCHA;
 
+import java.util.List;
+
 public class Captcha {
+
+    private String[] patternArray={"1","2"};
+    private String[] number={"1","2","3","4","5","6","7","8","9"};
+    private String[] operatorArray={"+","-","*","/"};
+    private String[] number1={"1","2","3","4","5","6","7","8","9"};
+
+    private int pattern;
+    private int leftOperand;
+    private int operater;
+    private int rightOperand;
 
     @Override
     public String toString() {
-        return "One + 1";
+        if(pattern==1)
+            return "One + 1";
+        return "";
+    }
+
+    public Captcha(int pattern, int leftOperand, int operater, int rightOperand) {
+        this.pattern = pattern;
+        this.leftOperand = leftOperand;
+        this.operater = operater;
+        this.rightOperand = rightOperand;
+    }
+
+    public int getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(int pattern) {
+        this.pattern = pattern;
+    }
+
+    public int getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(int leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public int getOperater() {
+        return operater;
+    }
+
+    public void setOperater(int operater) {
+        this.operater = operater;
+    }
+
+    public int getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(int rightOperand) {
+        this.rightOperand = rightOperand;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/captcha/CAPTCHA/captchaTestV1.java
+++ src/test/java/com/captcha/CAPTCHA/captchaTestV1.java
@@ -7,12 +7,12 @@ public class captchaTestV1 {
 
     @Test
     public void patternOneShouldGenerateOnePlus1(){
-        Captcha captcha = new Captcha();
+        Captcha captcha = new Captcha(1,1,1,1);
         Assert.assertEquals("One + 1",captcha.toString());
     }
 
     @Test
-    private void patternTwoShould() {
+    private void patternOneShould() {
 
     }
 
